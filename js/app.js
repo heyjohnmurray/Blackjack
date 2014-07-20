10d520a90b89059120036783b39fac04980278b3
John <jmurray@redventures.com>
2014-07-20 11:35:36 -0400

    getting started

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//inspiration: http://www.addictinggames.com/puzzle-games/blackjack.jsp
+
+//create deck
+var suits = ['diamond', 'heart', 'spade', 'club']
+var numbers = ['0,1,2,3,4,5,6,7,8,9,10,11,12,13']
+
+var random = Math.floor(Math.random() * 6) + 1
+
+alert(random);
