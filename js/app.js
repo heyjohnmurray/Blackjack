b676211a4512914f37a9fb34f761fdf5556e16b7
John <jmurray@redventures.com>
2014-07-20 13:08:31 -0400

    generate card and suit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 //inspiration: http://www.addictinggames.com/puzzle-games/blackjack.jsp
 
-//create deck
-var suits = ['diamond', 'heart', 'spade', 'club']
-var numbers = ['0,1,2,3,4,5,6,7,8,9,10,11,12,13']
+//generate suit
+var suits = ['diamonds','hearts','spades','clubs'];
+var randomSuit = Math.floor(Math.random() * 4);
 
-var random = Math.floor(Math.random() * 6) + 1
+//generate card number
+var cards = ['ace','one','two','three','four','five','six','seven','eight','nine','ten','jack','queen','king'];
+var randomCard = Math.floor(Math.random() * 13) + 1;
 
-alert(random);
+//your card is ...
+console.log(cards[randomCard] + " of " + suits[randomSuit]);
+
+//deal function
