f1c9f181b96fee15482eec6a7fc3def8dad4a835
John <jmurray@redventures.com>
2014-07-21 00:32:26 -0400

    reorganize card creation code

--- js/app.js
+++ js/app.js
@@ -1,16 +1,25 @@
 //inspiration: http://www.addictinggames.com/puzzle-games/blackjack.jsp
-function createCard (){
-  	//generate suit
-	var suits = ['diamonds','hearts','spades','clubs'];
-	var randomSuit = Math.floor(Math.random() * 4);
+
+function createCard(){
+
+	//generate suit
+  	function cardSuit (){
+  	
+		var suits = ['diamonds','hearts','spades','clubs'];
+		var randomSuit = Math.floor(Math.random() * 4);
+
+		return suits[randomSuit];	
+	}
 
 	//generate card number
-	var cards = ['ace','one','two','three','four','five','six','seven','eight','nine','ten','jack','queen','king'];
-	var randomCard = Math.floor(Math.random() * 13) + 1;
+	function cardNumber (){
+		var cards = ['one','two','three','four','five','six','seven','eight','nine','ten','jack','queen','king', 'ace'];
+		var randomCard = Math.floor(Math.random() * 13) + 1;
 
-	//your card is ...
-	return cards[randomCard] + " of " + suits[randomSuit];
-	//console.log(cards[randomCard] + " of " + suits[randomSuit])
+		return cards[randomCard];
+	}
+
+	return '<div class="suite">' + cardSuit() + '</div> <div class="number">' + cardNumber() + '</div>';
 }
 
 //when you hit the deal button :: not sure if this needs to be wrapped in a function
@@ -20,7 +29,6 @@ dealButton.addEventListener('click', function(e){
 
 	//cards to the dealer :: start with one card
 	document.querySelector('.dealer-cards .card-0').innerHTML = createCard();
-
 
 	//cards to the player :: start with two cards
 	document.querySelector('.player-cards .card-0').innerHTML = createCard();
