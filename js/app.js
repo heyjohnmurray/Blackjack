cf3d3b42373b177a967e76a23f7ef50fa666da93
John <jmurray@redventures.com>
2014-07-21 20:45:53 -0400

    get value of each player card

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ function createCard(){
 	//generate suit
   	function cardSuit (){
   	
-		var suits = ['diamonds','hearts','spades','clubs'];
+		var suits = ['&diamondsuit;','&heartsuit;','&spadesuit;','&clubsuit;'];
 		var randomSuit = Math.floor(Math.random() * 4);
 
 		return suits[randomSuit];	
@@ -13,13 +13,13 @@ function createCard(){
 
 	//generate card number
 	function cardNumber (){
-		var cards = ['one','two','three','four','five','six','seven','eight','nine','ten','jack','queen','king', 'ace'];
+		var cards = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
 		var randomCard = Math.floor(Math.random() * 13) + 1;
 
 		return cards[randomCard];
 	}
 
-	return '<div class="suite">' + cardSuit() + '</div> <div class="number">' + cardNumber() + '</div>';
+	return '<div class="number">' + cardNumber() + '</div><div class="suite">' + cardSuit() + '</div> ';
 }
 
 //when you hit the deal button :: not sure if this needs to be wrapped in a function
@@ -33,5 +33,10 @@ dealButton.addEventListener('click', function(e){
 	//cards to the player :: start with two cards
 	document.querySelector('.player-cards .card-0').innerHTML = createCard();
 	document.querySelector('.player-cards .card-1').innerHTML = createCard();
+
+	var dealtCards = document.querySelectorAll('.player-cards .card .number');
+	for (var i = 0; i < dealtCards.length; i++) {
+		console.log(dealtCards[i].innerHTML);
+	};
 
 }, false);
