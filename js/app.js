ee9b2dcd4b67f86b588e3a24cbb9fd67ac78397c
John Murray <jmurray@redventures.com>
2014-10-12 23:58:58 -0400

    get card deal function to spit out specified number of cards.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,181 @@
-// Trying to think about this the way they talk about it on Treehouse OOJS Pt 1 video tutorial
-
 // Necessary Ojbects
-	// deck :: 52 cards : 14cards : 4 suits : 2 red [clubs, spades] : 2 black [hearts, diamonds] :: Ace has 2 values (1, 11)
-	// deal :: check if cards have been dealt, then deal 2 cards
-	// 
+	// deck 	:: 52 cards : 14cards : 4 suits : 2 red [clubs, spades] : 2 black [hearts, diamonds] 
+				//	:: Ace has 2 values (1, 11)
+	// dealt 	:: check if cards have been dealt, then deal 2 cards
+	// value 	:: sum the value of the cards you have [this can be reused]
+	// hit		:: generate random new card that hasn't been dealt yet add its value to the existing cards
+	// stay		:: give dealer random numer of cards
+
+	// bet		:: allow user to choose value to wager
+				//	:: keep track of amount wagered (add multiple token clicks)
+				//	:: subtract wager from existing cash in hand (start out with 1500)
+
+//inspiration: http://www.addictinggames.com/puzzle-games/blackjack.jsp
+
+// players
+var dealer = document.querySelector('.dealer-cards');
+var player = document.querySelector('.player-cards');
+
+// buttons
+var dealButton = document.getElementById('deal-button');
+var hitButton = document.getElementById('hit-me');
+var stayButton = document.getElementById('stay');
+
+function createCard(){
+	// choose random suit
+	var suitType = Math.floor(Math.random() * 4); // generate random number between 0 and 3
+  		
+	// possible suits
+	var suits = { // originally this was an array, but an object lets me store more info for this
+		0: {
+			'name': 'diamonds',
+			'symbol': '&diamondsuit;',
+			'color': 'red'
+		},
+
+		1: {
+			'name': 'hearts',
+			'symbol': '&heartsuit;',
+			'color': 'red'
+		},
+
+		2: {
+			'name': 'clubs',
+			'symbol': '&clubsuit;',
+			'color': 'black'
+		},
+
+		3: {
+			'name': 'spades',
+			'symbol': '&spadesuit;',
+			'color': 'black'
+		}
+	}
+
+	// chose random card number
+	var cardNumber = Math.floor(Math.random() * 14) + 1; // generate random number between 1 and 14
+
+	// possible cards
+	var cards = {
+		1: {
+			'name': 'ace',
+			'face': 'A',
+			'value': 1
+		},
+
+		2: {
+			'name': 'two',
+			'face': '2',
+			'value': 2
+		},
+
+		3: {
+			'name': 'three',
+			'face': 3,
+			'value': 3
+		},
+
+		4: {
+			'name': 'four',
+			'face': '4',
+			'value': 4
+		},
+
+		5: {
+			'name': 'five',
+			'face': '5',
+			'value': 5
+		},
+
+		6: {
+			'name': 'six',
+			'face': '6',
+			'value': 6
+		},
+
+		7: {
+			'name': 'seven',
+			'face': '7',
+			'value': 7
+		},
+
+		8: {
+			'name': 'eight',
+			'face': '8',
+			'value': 8
+		},
+
+		9: {
+			'name': 'nine',
+			'face': '9',
+			'value': 9
+		},
+
+		10: {
+			'name': 'ten',
+			'face': '10',
+			'value': 10
+		},
+
+		11: {
+			'name': 'jack',
+			'face': 'J',
+			'value': 10
+		},
+
+		12: {
+			'name': 'queen',
+			'face': 'Q',
+			'value': 10
+		},
+
+		13: {
+			'name': 'king',
+			'face': 'K',
+			'value': 10
+		},
+
+		14: {
+			'name': 'ace',
+			'face': 'A',
+			'value': 11
+		}
+	}
+
+	return	'<div class="number ' + suits[suitType]['color'] + '">' + cards[cardNumber]['face'] + '</div>' + '<div class="suit ' + suits[suitType]['color'] +'">' + suits[suitType]['symbol'] + '</div>';
+} // close createCard()
+
+function dealCards(whichUser, cardsDealt){ // should accept user and number parameters	
+	for (var i = 0; i < cardsDealt; i++) {
+		var userName = whichUser.className;
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		whichUser.appendChild(newCard);
+		/* 
+			I also need it to add the createCard() code to the new card.
+				document.querySelector('.' + userName + ' .card').innerHTML = createCard(); 
+				I need it to print the value on EVERY card not just one
+		*/
+	};	
+}
+
+// Deal Button Click :: Only hit once. Hide after click. Can deal multiple cards
+dealButton.addEventListener('click', function(e){
+
+	// Deal mutliple cards 
+	dealCards(dealer, 1);
+	dealCards(player, 2);
+	
+	e.target.style.display = "none"; // Hide after click
+
+}, false);
+
+// Hit Button Click :: Only deal one card at a time
+hitButton.addEventListener('click', function(e){
+	dealCards(player, 1);
+}, false);
+
+// Stay Button Click
+stayButton.addEventListener('click', function(e){
+	// this should do math. create custom function to add up card values
+}, false);
