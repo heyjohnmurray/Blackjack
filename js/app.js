198391db4b23643f19dfb002db60c3b5f0e14d2d
John Murray <jmurray@redventures.com>
2014-10-15 23:30:18 -0400

    push

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+// http://jsbin.com/jisukibenaqo/2/edit
+
+
+
 // dom manipulation in one function
 // business logic in another
 
@@ -24,18 +28,19 @@ var player = document.querySelector('.player-cards');
 var dealButton = document.getElementById('deal-button');
 var hitButton = document.getElementById('hit-me');
 var stayButton = document.getElementById('stay');
+var card = cardInfo();
 var cardPoints = [];
 
 // new objects you need. :: try to find the notes james took on this and use them to create these constructors
-var Deck = function(){
+var Deck = {
 
 }
 
-var Player = function(){
+var Player = {
 
 }
 
-var Dealer = function(){
+var Dealer = {
 
 }
 
@@ -160,22 +165,36 @@ function cardInfo(){
 		}
 	};
 
-	var cardData = {
+	return {
 		color: suits[suitType]['color'],
 		face: cards[cardNumber]['face'],
 		suit: suits[suitType]['name'],
 		value: cards[cardNumber]['value'],
 		symbol: suits[suitType]['symbol']
-	};
-
-	//console.log(cardData);
-	return cardData; // return an object so you can keep this logic separate from the html-generated logic in cardRender()
+	}; // return an object so you can keep this logic separate from the html-generated logic in cardRender()
 
 }// close cardInfo()
 
-function cardRender(color, face, suit, value, symbol) { // creates the html that goes in the card
-	var card = cardInfo(); // bring in the cardInfo object that we generated.
-	return	'<div class="number ' + card.color + '">' + card.face + '</div>' + '<div class="suit ' + card.color +'">' + card.symbol + '</div>';
+var myCard = getNewCard();
+
+function getNewCard() {
+	return {
+		color: 'red',
+		face: 'hearts',
+		symbol: 'heartyshapedthing.png',
+		value: '12'
+	};
+}
+
+function renderMe(myCard) { // creates the html that goes in the card
+	console.log(myCard);
+}
+
+
+
+
+function cardRender(card) { // creates the html that goes in the card
+	//return	'<div class="number ' + card.color + '">' + card.face + '</div>' + '<div class="suit ' + card.color +'">' + card.symbol + '</div>';
 }
 
 function createCard(whichUser) { // deals a single card
@@ -190,11 +209,6 @@ function dealCards(whichUser, cardsDealt){ // deals multiple cards
 	for (var i = 0; i < cardsDealt; i++) {
 		createCard(whichUser);
 	}
-}
-
-function calcScore(whichUser, totalPoints) {
-	var card = cardInfo; // bring in the cardInfo object that we generated.
-	console.log(card); // PICK UP WHERE YOU LEFT OFF HERE
 }
 
 function scoreRender(whichUser) {
@@ -216,8 +230,7 @@ dealButton.addEventListener('click', function(e){
 // Hit Button Click :: Only deal one card at a time
 hitButton.addEventListener('click', function(e){
 
-	//createCard(player);
-	calcScore();
+	createCard(player);
 
 }, false);
 
