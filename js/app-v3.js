e62fb96128553cd5f8f77245acae76f149a3ccc6
John Murray <jmurray@redventures.com>
2014-11-07 22:10:06 -0500

    add function wrapper to application so that it can start to interact with the page

--- js/app-v3.js
+++ js/app-v3.js
@@ -13,282 +13,293 @@
 ////////////////////////
 // :: PLAYER LOGIC :: //
 ////////////////////////
+(function(undefined){
 
-// constructor :: controls Player and Dealer attributes
-function Player() {
-	this.cards = [];
-	this.score = 0;
-	this.wager = 0;
-}
-
-Player.prototype.receiveCard = function(card){
-	this.card.push(card);
-};
-
-// constructor
-function PlayerUI(name){
-	this.name = name;
-  	this.cardDom = undefined;
-  	this.scoreDom = undefined;
-}
-
-PlayerUI.prototype.setCardDom = function(element){
-  	this.cardDom = element;
-};
-
-PlayerUI.prototype.setScoreDom = function(element){
-  	this.scoreDom = element;
-};
-
-///////////////////////
-// :: WAGER LOGIC :: //
-///////////////////////
-
-// constructor
-function Betting (){
-	this.maxCashToStart = 1500;
-	this.cashLeftOver = undefined;
-	this.playerWager = undefined;
-}
-
-// betting actions
-Betting.prototype = {
-	constructor: Betting,
-	disableBets: function(){
-		// rewrite this ...
-		newDiv.className ='bets-off';
-		var firstItem = document.querySelector('.bets').firstChild;
-		document.querySelector('.bets').insertBefore(newDiv, firstItem);
-	},
-	updateWager: function(){
-		// rewrite this ...
-		document.querySelector('.wager-total .bet').innerHTML = playerWager;
-	}
-};
-
-function GameController() {
-// plopping these here for now b/c this is where they should end up.
-	// create Player 1
-	var playerOne = new Player();
-	var playerRender = new PlayerUI('John');
-
-	// create Dealer instance
-	var gameDealer = new Player();
-	var dealerRender = new PlayerUI('Dealer');
-
-	// create Deck instance
-	var myDeck = new Deck();
-}
-
-//////////////////////////
-// :: GAMEPLAY LOGIC :: //
-//////////////////////////
-
-// constructor
-
-// James, these dom elements being assigned to the constructor are very specific
-// does it make sense for them to be more modular. should i being making them accept
-// a parameter of 'element' or something like that instead of just assigning a dom element
-// to them so explicitly? 
-function GameUI(){
-	this.dealButton = document.getElementById('deal-button');
-	this.hitButton = document.getElementById('hit-me');
-	this.stayButton = document.getElementById('stay');
-	this.betAnchors = document.querySelector('.bets').getElementsByTagName('a');
-	this.resetButton = document.getElementById('reset');
-	this.gameActions = document.querySelector('.js-actions').classList.add('is-shown');
-}
-
-// attach all event listeners to this prototype
-GameUI.prototype = {
-	constructor: GameUI,
-	makeWager: function(){
-		// what happens when you click the chips?
-		for (var i = 0; i < this.betAnchors.length; i++) {
-			betAnchors[i].addEventListener('click', wagerEvents());
-		}
-	},
-	wagerEvents: function(e){
-		console.log('wager event!'); // test msg
-		// NOTES:
-		// deal button shows
-		this.gameActions.classList.add('is-shown');
-		// calculations are done : methods related to Wager proto
-
-		// default behavior stopped
-		e.preventDefault();
-	},
-	dealAction: function(){
-		// NOTES:
-	  	// what happens when you click the deal button?
-	  	// other UI elements appear
-	  	// cards are dealt to player and dealer
-	  	// wagering is disabled
-	},
-	stayAction: function(){
-		// NOTES:
-	  	// what happens when you click the stay button 
-	  	// save user player points value
-		// give dealer a card then test whether it's greater or less than player's score
-	}
-};
-
-// constructor
-function Card(definedCard) {
-	// this ensures that if we're not passed an object, we throw an error
-	if (definedCard !== undefined && 'object' !== typeof definedCard) {
-		throw new Error('');
+	// constructor :: controls Player and Dealer attributes
+	function Player() {
+		this.cards = [];
+		this.score = 0;
+		this.wager = 0;
 	}
 
-	this.suit = definedCard.suit;
-	this.symbol = definedCard.symbol;
-	this.color = definedCard.color;
-	this.face = definedCard.face;
-	this.name = definedCard.name;
-	this.value = definedCard.value;
-}
+	Player.prototype.receiveCard = function(card){
+		this.card.push(card);
+	};
 
-Card.prototype = {
-	constructor: Card,
-	// card methods next ...
-};
-
-// constructor
-function Deck(){
-	this.suits = {
-		0: {
-			'suit': 'diamonds',
-			'symbol': '&diamondsuit;',
-			'color': 'red'
-		},
-
-		1: {
-			'suit': 'hearts',
-			'symbol': '&heartsuit;',
-			'color': 'red'
-		},
-
-		2: {
-			'suit': 'clubs',
-			'symbol': '&clubsuit;',
-			'color': 'black'
-		},
-
-		3: {
-			'suit': 'spades',
-			'symbol': '&spadesuit;',
-			'color': 'black'
-		}
-	}; // close suits
-
-	this.cards = {
-		1: {
-			'name': 'ace',
-			'face': 'A',
-			'value': 1
-		},
-
-		2: {
-			'name': 'two',
-			'face': '2',
-			'value': 2
-		},
-
-		3: {
-			'name': 'three',
-			'face': 3,
-			'value': 3
-		},
-
-		4: {
-			'name': 'four',
-			'face': '4',
-			'value': 4
-		},
-
-		5: {
-			'name': 'five',
-			'face': '5',
-			'value': 5
-		},
-
-		6: {
-			'name': 'six',
-			'face': '6',
-			'value': 6
-		},
-
-		7: {
-			'name': 'seven',
-			'face': '7',
-			'value': 7
-		},
-
-		8: {
-			'name': 'eight',
-			'face': '8',
-			'value': 8
-		},
-
-		9: {
-			'name': 'nine',
-			'face': '9',
-			'value': 9
-		},
-
-		10: {
-			'name': 'ten',
-			'face': '10',
-			'value': 10
-		},
-
-		11: {
-			'name': 'jack',
-			'face': 'J',
-			'value': 10
-		},
-
-		12: {
-			'name': 'queen',
-			'face': 'Q',
-			'value': 10
-		},
-
-		13: {
-			'name': 'king',
-			'face': 'K',
-			'value': 10
-		},
-
-		14: {
-			'name': 'ace',
-			'face': 'A',
-			'value': 11
-		}
-	}; // close cards
-}
-
-Deck.prototype = {
-	constructor: Deck,
-	dealRandomCard: function(){
-		var suitType = Math.floor(Math.random() * 4);
-		var cardNumber = Math.floor(Math.random() * 13) + 1;
-
-		var randomCard = new Card({
-			suit: this.suits[suitType].suit,
-			symbol: this.suits[suitType].symbol,
-			color: this.suits[suitType].color,
-			face: this.cards[cardNumber].face,
-			name: this.cards[cardNumber].name,
-			value: this.cards[cardNumber].value
-		});
-
-		console.log(randomCard);
-		return randomCard;
-	},
-	dealCards: function(user, number){
-		for (var i = 0; i < number; i++) {
-			this.dealRandomCard(user);
-		}
+	// constructor
+	function PlayerUI(name){
+		this.name = name;
+	  	this.cardDom = undefined;
+	  	this.scoreDom = undefined;
 	}
-};
+
+	PlayerUI.prototype.setCardDom = function(element){
+	  	this.cardDom = element;
+	};
+
+	PlayerUI.prototype.setScoreDom = function(element){
+	  	this.scoreDom = element;
+	};
+
+	///////////////////////
+	// :: WAGER LOGIC :: //
+	///////////////////////
+
+	// constructor
+	function Betting (){
+		this.maxCashToStart = 1500;
+		this.cashLeftOver = undefined;
+		this.playerWager = undefined;
+	}
+
+	// betting actions
+	Betting.prototype = {
+		constructor: Betting,
+		disableBets: function(){
+			// rewrite this ...
+			newDiv.className ='bets-off';
+			var firstItem = document.querySelector('.bets').firstChild;
+			document.querySelector('.bets').insertBefore(newDiv, firstItem);
+		},
+		updateWager: function(){
+			// rewrite this ...
+			document.querySelector('.wager-total .bet').innerHTML = playerWager;
+		}
+	};
+
+	function GameController() {
+	// plopping these here for now b/c this is where they should end up.
+		// create Player 1
+		var playerOne = new Player();
+		var playerRender = new PlayerUI('John');
+
+		// create Dealer instance
+		var gameDealer = new Player();
+		var dealerRender = new PlayerUI('Dealer');
+
+		// create Deck instance
+		var myDeck = new Deck();
+	}
+
+	//////////////////////////
+	// :: GAMEPLAY LOGIC :: //
+	//////////////////////////
+
+	// constructor
+	function GameUI(){
+		// this gets the ball rolling
+		// a new instance of this is called at the bottom of this page
+		this.registerDomElements();
+		this.registerWagerEvents();
+	}
+
+	// attach all event listeners to this prototype
+	GameUI.prototype = {
+		constructor: GameUI,
+		registerDomElements: function(){
+		  	this.dealButton = document.getElementById('deal-button');
+			this.hitButton = document.getElementById('hit-me');
+			this.stayButton = document.getElementById('stay');
+			this.betAnchors = document.querySelector('.bets').getElementsByTagName('a');
+			this.resetButton = document.getElementById('reset');
+			this.gameActions = document.querySelector('.js-actions');
+		},
+		registerWagerEvents: function(){
+			
+			// helps keep the scope of this on the GameUI prototype 
+			// instead of the click target
+			var scope = this;
+			function localWager(e) {
+				scope.wagerEvents(e);
+			}
+
+			// what happens when you click the chips?
+			for (var i = 0; i < this.betAnchors.length; i++) {
+				this.betAnchors[i].addEventListener('click', localWager);
+			}
+		},
+		wagerEvents: function(e){
+			// deal button shows
+			this.gameActions.classList.add('is-shown')
+			// calculations are done : methods related to Wager proto
+
+			// default behavior stopped
+			e.preventDefault();
+		},
+		dealAction: function(){
+			// NOTES:
+		  	// what happens when you click the deal button?
+		  	// other UI elements appear
+		  	// cards are dealt to player and dealer
+		  	// wagering is disabled
+		},
+		stayAction: function(){
+			// NOTES:
+		  	// what happens when you click the stay button 
+		  	// save user player points value
+			// give dealer a card then test whether it's greater or less than player's score
+		}
+	};
+
+	// constructor
+	function Card(definedCard) {
+		// this ensures that if we're not passed an object, we throw an error
+		if (definedCard !== undefined && 'object' !== typeof definedCard) {
+			throw new Error('');
+		}
+
+		this.suit = definedCard.suit;
+		this.symbol = definedCard.symbol;
+		this.color = definedCard.color;
+		this.face = definedCard.face;
+		this.name = definedCard.name;
+		this.value = definedCard.value;
+	}
+
+	Card.prototype = {
+		constructor: Card,
+		// card methods next ...
+	};
+
+	// constructor
+	function Deck(){
+		this.suits = {
+			0: {
+				'suit': 'diamonds',
+				'symbol': '&diamondsuit;',
+				'color': 'red'
+			},
+
+			1: {
+				'suit': 'hearts',
+				'symbol': '&heartsuit;',
+				'color': 'red'
+			},
+
+			2: {
+				'suit': 'clubs',
+				'symbol': '&clubsuit;',
+				'color': 'black'
+			},
+
+			3: {
+				'suit': 'spades',
+				'symbol': '&spadesuit;',
+				'color': 'black'
+			}
+		}; // close suits
+
+		this.cards = {
+			1: {
+				'name': 'ace',
+				'face': 'A',
+				'value': 1
+			},
+
+			2: {
+				'name': 'two',
+				'face': '2',
+				'value': 2
+			},
+
+			3: {
+				'name': 'three',
+				'face': 3,
+				'value': 3
+			},
+
+			4: {
+				'name': 'four',
+				'face': '4',
+				'value': 4
+			},
+
+			5: {
+				'name': 'five',
+				'face': '5',
+				'value': 5
+			},
+
+			6: {
+				'name': 'six',
+				'face': '6',
+				'value': 6
+			},
+
+			7: {
+				'name': 'seven',
+				'face': '7',
+				'value': 7
+			},
+
+			8: {
+				'name': 'eight',
+				'face': '8',
+				'value': 8
+			},
+
+			9: {
+				'name': 'nine',
+				'face': '9',
+				'value': 9
+			},
+
+			10: {
+				'name': 'ten',
+				'face': '10',
+				'value': 10
+			},
+
+			11: {
+				'name': 'jack',
+				'face': 'J',
+				'value': 10
+			},
+
+			12: {
+				'name': 'queen',
+				'face': 'Q',
+				'value': 10
+			},
+
+			13: {
+				'name': 'king',
+				'face': 'K',
+				'value': 10
+			},
+
+			14: {
+				'name': 'ace',
+				'face': 'A',
+				'value': 11
+			}
+		}; // close cards
+	}
+
+	Deck.prototype = {
+		constructor: Deck,
+		dealRandomCard: function(){
+			var suitType = Math.floor(Math.random() * 4);
+			var cardNumber = Math.floor(Math.random() * 13) + 1;
+
+			var randomCard = new Card({
+				suit: this.suits[suitType].suit,
+				symbol: this.suits[suitType].symbol,
+				color: this.suits[suitType].color,
+				face: this.cards[cardNumber].face,
+				name: this.cards[cardNumber].name,
+				value: this.cards[cardNumber].value
+			});
+
+			console.log(randomCard);
+			return randomCard;
+		},
+		dealCards: function(user, number){
+			for (var i = 0; i < number; i++) {
+				this.dealRandomCard(user);
+			}
+		}
+	};
+
+	var myGame = new GameUI();
+})();
