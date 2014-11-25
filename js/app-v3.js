a502acb0c8a24fb248df53819c5c21cea795e08f
John Murray <jmurray@redventures.com>
2014-11-25 01:17:11 -0500

    one more note

--- js/app-v3.js
+++ js/app-v3.js
@@ -1,14 +1,15 @@
 // still need to:
-//	declare a winner (auto declare if one or more player goes over 21) -- game controller logic / game ui render
-//	declare a draw if one occurs -- game controller logic / game ui render
-//	manipulate dom after winner declared -- game 
-//	update player score after 'stay' is hit and winner is determined -- game ui logic
-//	declare playerOne 'winner' if they draw 21 immediately after 'deal'
-//	remove duplicate card creation (check suit and face or name of each card in the array to the one created, force new card creation if duplicate) -- game controller logic / game ui render
-//	if ace exists in the array, then make second ace worth 1 point (either that or prompt player for desired value -- 11 or 1?)
-//	make dealer ask for more cards if need be. right now he only ever gets one.
-//		so : 	save user player points value
-//				give dealer a card then test whether it's greater or less than player's score
+	//	declare a winner (auto declare if one or more player goes over 21) -- game controller logic / game ui render
+	//	declare a draw if one occurs -- game controller logic / game ui render
+	//	manipulate dom after winner declared -- game 
+	//	update player score after 'stay' is hit and winner is determined -- game ui logic
+	//	declare playerOne 'winner' if they draw 21 immediately after 'deal'
+	//	remove duplicate card creation (check suit and face or name of each card in the array to the one created, force new card creation if duplicate) -- game controller logic / game ui render
+	//	if ace exists in the array, then make second ace worth 1 point (either that or prompt player for desired value -- 11 or 1?)
+	//	make dealer ask for more cards if need be. right now he only ever gets one.
+	//		so : 	save user player points value
+	//				give dealer a card then test whether it's greater or less than player's score
+	//	enhance the UI a bit by  adding animations and stuff on the front end. basically, spruce it up a bit.
 
 ////////////////////////
 // :: PLAYER LOGIC :: //
