ded0fa0cebf75131a55736f68dce849ecfad71d7
John Murray <jmurray@redventures.com>
2014-11-27 01:27:17 -0500

    commit what i have so far for result tracking prototype

--- js/app-v3.js
+++ js/app-v3.js
@@ -229,24 +229,24 @@
 /////////////////////////
 // :: RESULTS LOGIC :: //
 /////////////////////////
-	// function Result (){
-	// 	this.winner = undefined;
-	// 	this.playerScore = undefined;
-	// 	this.dealerScore = undefined;
-	// }
+	function Result (){
+		this.winner = undefined;
+		this.playerScore = undefined;
+		this.dealerScore = undefined;
+	}
 
-	// Result.prototype = {
-	// 	constructor: Result,
-	// 	decision: function(){
-	// 		if (this.playerScore > this.dealerScore) {
-	// 			this.winner = this.playerOne.name;
-	// 		} else if (this.playerScore < this.dealerScore) {
-	// 			this.winner = this.gameDealer.name;
-	// 		}
-	// 		console.log(this.winner);
-	// 		return this.winner;
-	// 	}
-	// };
+	Result.prototype = {
+		constructor: Result,
+		decision: function(){
+			if (this.playerScore > this.dealerScore) {
+				this.winner = this.playerOne.name;
+			} else if (this.playerScore < this.dealerScore) {
+				this.winner = this.gameDealer.name;
+			}
+			console.log(this.winner);
+			return this.winner;
+		}
+	};
 
 ///////////////////////
 // :: WAGER LOGIC :: //
@@ -278,7 +278,7 @@
 		this.betObj = new Betting();
 		this.playerOne = new Player('John','playerOne');
 		this.gameDealer = new Player('Dealer','gameDealer');
-		//this.result = new Result();
+		this.result = new Result();
 	}
 
 	function GameUI(){ // this gets the ball rolling a new instance of GameUI is called at the bottom of this page
@@ -445,36 +445,21 @@
 			}
 			this.stayButton.addEventListener('click', localStayEvent);
 		},
-		decideWinner: function(){
-			var winner;
-			var playerScore = this.gameController.playerOne.getScore();
-			var dealerScore = this.gameController.gameDealer.getScore();
-			if (playerScore > dealerScore) {
-				winner = this.gameController.playerOne.name  + ' wins!';
-			} else if (playerScore < dealerScore) {
-				winner = this.gameController.gameDealer.name  + ' wins!';
-			} else {
-				winner = 'Looks like it\'s a draw';
-			}
-			return winner;
-		},
 		announceWinner: function(){
 			var winner = this.decideWinner();
 			document.querySelector('.winner-is p').innerHTML = winner;
 		},
 		stayEvent: function(){
 			var dealerCards = this.gameController.gameDealer.getCards();
-			// var playerScore = this.gameController.playerOne.getScore();
-			// var dealerScore = this.gameController.gameDealer.getScore();
+			var playerScore = this.gameController.playerOne.getScore();
+			var dealerScore = this.gameController.gameDealer.getScore();
 
 			this.hitButton.style.display = 'none';
 			this.stayButton.style.display = 'none';
 			this.gameController.myDeck.dealCards(this.gameController.gameDealer,1);
 			this.createCard(dealerCards[dealerCards.length-1], this.gameController.gameDealer);
 			this.renderUpdatedScore();
-			//this.gameController.result.decision();
-			this.decideWinner();
-			this.announceWinner();
+			this.gameController.result.decision();
 		}
 	};
 
