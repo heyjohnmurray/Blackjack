d17021d9e331270f640d13146cb364d50dc9fb59
John Murray <jmurray@redventures.com>
2014-11-27 02:48:56 -0500

    remove 4 items from to-do list. FUCK YEAH

--- js/app-v3.js
+++ js/app-v3.js
@@ -1,8 +1,4 @@
 // still need to:
-	//	declare a winner (auto declare if one or more player goes over 21) -- game controller logic / game ui render
-	//	declare a draw if one occurs -- game controller logic / game ui render
-	//	manipulate dom after winner declared -- game ui
-	//	update player score after 'stay' is hit and winner is determined -- game ui logic
 	//	declare playerOne 'winner' if they draw 21 immediately after 'deal'
 	//	remove duplicate card creation (check suit and face or name of each card in the array to the one created, force new card creation if duplicate) -- game controller logic / game ui render
 	//	if ace exists in the array, then make second ace worth 1 point (either that or prompt player for desired value -- 11 or 1?)
